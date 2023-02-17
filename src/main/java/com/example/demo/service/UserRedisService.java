11cfdc77abe24425f67693b626ab75b30a0a1129
cliu <cl2037829916@gmail.com>
2023-02-17 07:33:17 +0000

    Redis can save Signed-off-by: cliu <cl2037829916@gmail.com>

--- src/main/java/com/example/demo/service/UserRedisService.java
+++ src/main/java/com/example/demo/service/UserRedisService.java
@@ -2,13 +2,13 @@ package com.example.demo.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.util.concurrent.TimeUnit;
 @Service
 public class UserRedisService {
     @Autowired
-    private RedisTemplate redisTemplate;
+    private StringRedisTemplate redisTemplate;
 
     public void set(String name, String role){
         redisTemplate.opsForValue().set(name, role, 100, TimeUnit.SECONDS);
